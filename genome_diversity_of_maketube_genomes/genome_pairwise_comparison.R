13613fcdc764453117259ace05ec7ff925dae8a4
Adrien Le Meur <71882657+adrienlemeur@users.noreply.github.com>
2025-06-17 10:59:23 +0200

    first commit

---
+++ genome_diversity_of_maketube_genomes/genome_pairwise_comparison.R
@@ -0,0 +1,86 @@
+{
+  rm(list = ls())
+  gc()
+  graphics.off()
+  library(ggplot2)
+  library(scales)
+  library(ggrepel)
+  library(ggforce)
+}
+
+lineages_color <- c(L1 = "darkorchid1", L2 = "dodgerblue2", L3 = "blueviolet",L4 = "firebrick2",L5 =  "darkorange4" ,L6 = "darkgreen",L7 = "gold1", bovis = "black")
+
+dnadiff_data <- read.table("distance_to_reference.tsv", header = T, sep = "\t")
+colnames(dnadiff_data) <- c("reference", "reference_lineage", "reference_source", "sample", "sample_lineage", "sample_source", "ref_length", "ref_aligned", "sample_length", "sample_aligned")
+
+dnadiff_data$relationship <- dnadiff_data$sample_lineage == dnadiff_data$reference_lineage
+dnadiff_data$relationship[dnadiff_data$relationship == TRUE] <- "intralineage"
+dnadiff_data$relationship[dnadiff_data$relationship == FALSE] <- "interlineage"
+
+dnadiff_data$sample_source <- factor(dnadiff_data$sample_source, levels = c("natural", "snpmutator", "maketube"), ordered = T)
+dnadiff_data$lineage_source <- paste(dnadiff_data$sample_lineage, dnadiff_data$sample_source, sep = ", ")
+
+sub1 <- subset(dnadiff_data, !(sample_source == "snpmutator" & sample_lineage == "L2"))
+
+subset_dnadiff <- subset(sub1,
+                          (reference == "H37Rv" & sample_source == "natural") |
+#                          (reference_lineage == sample_lineage & sample_source == "natural") |
+                          (sample_source != "natural" & reference_lineage == sample_lineage)
+                        )
+
+fig3_A <- ggplot(data = subset_dnadiff) +
+  geom_hline(yintercept = 0.001,
+             linetype = "dotted", color = "red", ) +
+  geom_hline(yintercept = seq(0, 0.013, by = 0.001),
+             linetype = "dotted", color = "grey", ) +
+  geom_vline(
+    xintercept = seq(0, 0.013, by = 0.001),
+    linetype = "dotted", color = "grey", ) +
+  geom_point(aes(x = as.numeric(1-ref_aligned/ref_length), y = as.numeric(1-sample_aligned/sample_length), fill = sample_lineage, pch = sample_source), size = 2) +
+  geom_mark_hull(aes(x = 1-ref_aligned/ref_length, y = 1-sample_aligned/sample_length, col = sample_source, label = sample_source),
+                 label.hjust = 0, fill = "grey",
+                 con.type = "elbow", concavity = 10, expand = 0.03, radius = 0.03,
+                 label.fontface = "bold", label.fontsize = 13,
+                 con.colour = "inherit", label.fill = NA
+  ) +
+  scale_shape_manual(values = c(natural = 21, snpmutator = 24, maketube = 25), name = "Genome category :") +
+  scale_fill_manual(values = lineages_color, name = "Strain lineage") +
+  scale_color_manual(values = c("maketube" = "firebrick", "snpmutator" = "orange", "natural" = "darkgreen"), guide = "none") +
+  guides(fill = guide_legend(override.aes = list(pch = 21)), 
+         pch = guide_legend(order = 1)) +
+  xlim(c(-0.001, 0.013)) + ylim(c(-0.001, 0.013)) +
+  ylab("Distance to the reference") +
+  xlab("Distance to the sample") +
+  theme_light() +
+  theme(axis.title.y = element_text(size = 14, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(size = 14, face = "bold", hjust = 0.5),
+        legend.text = element_text(size = 12), legend.title = element_text(size = 14, face = "bold"))
+fig3_A
+
+svg("fig3_A_pairwise_nucleotide_distance.svg", width = 10, height = 10)
+fig3_A
+dev.off()
+
+svg("fig3_B_pairwise_nucleotide_distribution.svg")
+ggplot(subset(subset_dnadiff, relationship == "intralineage")) +
+  geom_vline(
+              xintercept = seq(0, 0.013, by = 0.001),
+              linetype = "dotted", color = "grey") +
+  geom_boxplot(aes(sample_source, ((1-ref_aligned/ref_length) + (1-sample_aligned/sample_length))/2, fill = sample_source)) +
+  scale_fill_manual(values = c("maketube" = "firebrick", "snpmutator" = "orange", "natural" = "darkgreen"), guide = "none") +
+  xlab("") + ylab("Average pairwise \ndistance to H37Rv") +
+  theme(axis.title.y = element_text(size = 14, hjust = 0.5),
+        axis.text.x = element_text(size = 14, hjust = 0.5),
+        legend.text = element_text(size = 12), legend.title = element_text(size = 14, face = "bold"))
+dev.off()
+subset(dnadiff_data)
+
+natural_strains <- subset_dnadiff[subset_dnadiff$sample_source == "maketube",]
+100*summary((1- natural_strains$sample_aligned / natural_strains$sample_length) + (1- natural_strains$ref_aligned / natural_strains$ref_length)/2)
+100*summary(1- natural_strains$sample_aligned / natural_strains$sample_length)
+100*summary(1- natural_strains$ref_aligned / natural_strains$ref_length)
+  #intralineage_maketube <- subset(subset_dnadiff, relationship == "intralineage" & sample_source == "maketube")
+#summary(((1-intralineage_maketube$ref_aligned/intralineage_maketube$ref_length) + (1-intralineage_maketube$sample_aligned/intralineage_maketube$sample_length))/2)
+
+
+
---
+++ genome_diversity_of_maketube_genomes/maketube_with_natural_genomes.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+if [ "$1" == "-r" ]; then
+	echo "rerun the complete analysis"
+
+	cat REAL_STRAINS/*.fasta > BLAST_DB/genome_concatenation.fasta
+	makeblastdb -in BLAST_DB/genome_concatenation.fasta -title all_tuberculosis_genomes -dbtype nucl -out BLAST_DB/genome_concatenation
+	blastn -query BLAST_DB/IS6110.fasta -db BLAST_DB/genome_concatenation -outfmt "6 qseqid sseqid pident sstart send" > BLAST_DB/IS6110_query_results.tsv
+
+	while read reference_line
+	do
+		reference=$(echo $reference_line | cut -f1 -d ' ')
+		reference_file=$(echo $reference_line | cut -f3 -d ' ')
+		reference_transposon=$(echo $reference_line | cut -f4 -d ' ')
+		reference_lineage=$(echo $reference_line | cut -f5 -d ' ')
+
+		Rscript maketube.R \
+			--reference $reference_file --transposon $reference_transposon --nonhomoseq "REF/nonH37Rv_pool_sequence_cp.fasta" \
+			--output "maketube_run/${reference}_run" \
+			--structural_variants 5 --haplotype_count 10 --pop_count 1 \
+			--unmuted \
+			--mutation_rate 1.23e-7 --scaling_factor 0.125 \
+			--effective_pop 700 \
+			--TCAGI "0.172,0.329,0.172,0.329,0" \
+			--ABCDEF "0.65,0.05,0.21,0.27,0.02,0.65" \
+			--slope "50,100,150,200,250,300"
+
+		snpmutator "$reference_file" \
+			--num-simulations 15 \
+			--num-substitutions 800 \
+			--num-insertions 50 \
+			--num-deletions 50 \
+			--mono \
+			--seqid "${reference}" \
+			--fasta-dir "snpmutator_run/${reference}"
+
+	done < <(grep "reference_genome" natural_strains_info.txt | grep -v "#")
+fi
+
+mkdir -p DELTAS BAM
+
+while read reference_line
+do
+	reference=$(echo $reference_line | cut -f1 -d ' ')
+	reference_file=$(echo $reference_line | cut -f3 -d ' ')
+	reference_lineage=$(echo $reference_line | cut -f5 -d ' ')
+	reference_source=$(echo $reference_line | cut -f6 -d ' ' | sed -E 's/.$//')
+
+	while read sample_line
+	do
+
+		sample=$(echo $sample_line | cut -f1 -d ' ')
+		sample_file=$(echo $sample_line | cut -f3 -d ' ')
+		sample_lineage=$(echo $sample_line | cut -f5 -d ' ')
+		sample_source=$(echo $sample_line | cut -f6 -d ' ' | sed -E 's/.$//')
+
+		if [ ! -f "DELTAS/${sample}_${reference}.delta" ]
+		then
+			nucmer -p "DELTAS/${sample}_${reference}" $reference_file $sample_file 2> /dev/null
+			delta-filter -1 "DELTAS/${sample}_${reference}.delta" > "DELTAS/${sample}_${reference}.filter"
+		fi
+
+		if [ ! -f "DELTAS/${sample}_${reference}.report" ]
+		then
+			dnadiff -d "DELTAS/${sample}_${reference}.delta" -p "DELTAS/${sample}_${reference}" 2> /dev/null
+		fi
+
+		ref_length=$(sed -E 's/\s+/\t/g' "DELTAS/${sample}_${reference}.report" | grep "TotalBases" | cut -f2)
+		seq_length=$(sed -E 's/\s+/\t/g' "DELTAS/${sample}_${reference}.report" | grep "TotalBases" | cut -f3)
+
+		aligned_ref=$(head -n20 "DELTAS/${sample}_${reference}.report" | sed -E 's/\s+/\t/g' | grep "AlignedBases" | cut -f2 | cut -f1 -d '(')
+		aligned_sample=$(head -n20 "DELTAS/${sample}_${reference}.report" | sed -E 's/\s+/\t/g' | grep "AlignedBases" | cut -f3 | cut -f1 -d '(')
+
+		echo -e $reference $reference_lineage $reference_source $sample $sample_lineage  $sample_source $ref_length $aligned_ref $seq_length $aligned_sample | sed 's/ /\t/g'
+
+	done < <(cat natural_strains_info.txt | grep -v '#' | grep "sample")
+done < <(cat natural_strains_info.txt | grep -v '#' | grep "reference_genome")
+
