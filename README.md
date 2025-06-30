6bc90fbc08e8e016e31ea3c2d1b3510cd6b22025
Adrien Le Meur <alemeur@biophylo.com>
2025-06-30 11:45:22 +0200

    arborescence + comments

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-yes
+This is the repository for the maketube paper complete data.
+It includes : 
+	- the sequences used (with the exception of larger files)
+	- the script used for the analysis
+	- the figures & the script used to generate the figures.
+
+Exhaustive description :
+
+├── genome_diversity_of_maketube_genomes			# analysis of sequence diversity between natural and artificial genome
+│   ├── REF
+│   ├── DELTAS							# nucmer intermediary files .delta, .snps, etc.
+│   ├── artificial_strains					# snpmutator and maketube genomes
+│   ├── REAL_STRAINS						# natural strains sequences (& IS6110 locations, unused)
+│   ├── maketube_run						# maketube files : sequence, evolution partitions, annotations files. 
+|   │   ├── AF2122
+│   │   ├── 18b
+│   │   └── H37Rv
+│   └── maketube_with_natural_genomes.sh
+└────── natural_strains_info.txt				#ID, lineage and additional information about natural strains
+
--- genome_diversity_of_maketube_genomes/maketube_with_natural_genomes.sh
+++ genome_diversity_of_maketube_genomes/maketube_with_natural_genomes.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
+
+#can be skipped altogether, all necessary files are precomputed
 if [ "$1" == "-r" ]; then
 	echo "rerun the complete analysis"
 
+	#should be run once, the blast db file was too big
 	cat REAL_STRAINS/*.fasta > BLAST_DB/genome_concatenation.fasta
 	makeblastdb -in BLAST_DB/genome_concatenation.fasta -title all_tuberculosis_genomes -dbtype nucl -out BLAST_DB/genome_concatenation
 	blastn -query BLAST_DB/IS6110.fasta -db BLAST_DB/genome_concatenation -outfmt "6 qseqid sseqid pident sstart send" > BLAST_DB/IS6110_query_results.tsv
 
+	#parse reference line by line from natural_strains_infos
+	#for each genome generate a run of 
 	while read reference_line
 	do
 		reference=$(echo $reference_line | cut -f1 -d ' ')
 		reference_file=$(echo $reference_line | cut -f3 -d ' ')
 		reference_transposon=$(echo $reference_line | cut -f4 -d ' ')
 		reference_lineage=$(echo $reference_line | cut -f5 -d ' ')
-
+		
+		#maketube genome are manually copy pasted
 		Rscript maketube.R \
 			--reference $reference_file --transposon $reference_transposon --nonhomoseq "REF/nonH37Rv_pool_sequence_cp.fasta" \
 			--output "maketube_run/${reference}_run" \
-			--structural_variants 5 --haplotype_count 10 --pop_count 1 \
+			--structural_variants 3 --haplotype_count 10 --pop_count 1 \
 			--unmuted \
 			--mutation_rate 1.23e-7 --scaling_factor 0.125 \
 			--effective_pop 700 \
@@ -38,6 +44,7 @@ fi
 
 mkdir -p DELTAS BAM
 
+#compare all genome to the 3 reference genome annotated reference in natural_strains_info
 while read reference_line
 do
 	reference=$(echo $reference_line | cut -f1 -d ' ')
@@ -53,6 +60,8 @@ do
 		sample_lineage=$(echo $sample_line | cut -f5 -d ' ')
 		sample_source=$(echo $sample_line | cut -f6 -d ' ' | sed -E 's/.$//')
 
+
+		#all done accordingly to nucmer documentation
 		if [ ! -f "DELTAS/${sample}_${reference}.delta" ]
 		then
 			nucmer -p "DELTAS/${sample}_${reference}" $reference_file $sample_file 2> /dev/null
@@ -64,9 +73,11 @@ do
 			dnadiff -d "DELTAS/${sample}_${reference}.delta" -p "DELTAS/${sample}_${reference}" 2> /dev/null
 		fi
 
+		#grep aligned bases and total bases to avoid the nucmer rounding
 		ref_length=$(sed -E 's/\s+/\t/g' "DELTAS/${sample}_${reference}.report" | grep "TotalBases" | cut -f2)
 		seq_length=$(sed -E 's/\s+/\t/g' "DELTAS/${sample}_${reference}.report" | grep "TotalBases" | cut -f3)
 
+		#clean up the report line to extract only the value
 		aligned_ref=$(head -n20 "DELTAS/${sample}_${reference}.report" | sed -E 's/\s+/\t/g' | grep "AlignedBases" | cut -f2 | cut -f1 -d '(')
 		aligned_sample=$(head -n20 "DELTAS/${sample}_${reference}.report" | sed -E 's/\s+/\t/g' | grep "AlignedBases" | cut -f3 | cut -f1 -d '(')
 
